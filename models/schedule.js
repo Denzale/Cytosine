545ba12e0461b46c4172f72f4b471b32de1b415d
Denzale Houston <denzalethouston@gmail.com>
2021-01-26 21:45:10 -0700

    react practice removal

--- models/schedule.js
+++ models/schedule.js
@@ -1,33 +1,59 @@
-const db = require('../models');
-const router = require('express').Router();
-const isAuthenticated = require('../utils/middleware').isAuthenticated;
-router.get('/', isAuthenticated, function(req, res) {
-    // we can pass in things in the query of a REST call!
-    db.Schedule.findAll(req.query)
-        .then(dbModel => res.json(dbModel))
-        .catch(err => res.status(422).json(err));
-});
-router.get('/:id', isAuthenticated, function(req, res) {
-    db.Schedule.findByPk(req.params.id)
-        .then(dbModel => res.json(dbModel))
-        .catch(err => res.status(422).json(err));
-});
-router.post('/', isAuthenticated, function(req, res) {
-    db.Schedule.create({
-        UserId: req.user.id,
-        ...req.body
-    })
-        .then(dbModel => res.json(dbModel))
-        .catch(err => res.status(422).json(err));
-});
-router.put('/:id', isAuthenticated, function(req, res) {
-    db.Schedule.update(req.body, { where: { id: req.params.id }})
-        .then(dbModel => res.json(dbModel))
-        .catch(err => res.status(422).json(err));
-});
-router.delete('/:id', isAuthenticated, function(req, res) {
-    db.Schedule.destroy({ where: { id: req.params.id }})
-        .then(dbModel => res.json(dbModel))
-        .catch(err => res.status(422).json(err));
-});
-module.exports = router;
+module.exports = function (sequelize, DataTypes) {
+    const Schedule = sequelize.define('Schedule', {
+        sunday: {
+            type: DataTypes.DATE,
+            allowNull: false,
+            validate: {
+                len: [1]
+            }
+        },
+        monday: {
+            type: DataTypes.DATE,
+            allowNull: false,
+            validate: {
+                len: [1]
+            }
+        },
+        tuesday: {
+            type: DataTypes.DATE,
+            allowNull: false,
+            len: [1]
+        },
+        wednesday: {
+            type: DataTypes.DATE,
+            allowNull: false,
+            validate: {
+                len: [1]
+            }
+        },
+        thursday: {
+            type: DataTypes.DATE,
+            allowNull: false,
+            validate: {
+                len: [1]
+            }
+        },
+        friday: {
+            type: DataTypes.DATE,
+            allowNull: false,
+            validate: {
+                len: [1]
+            }
+        },
+        saturday: {
+            type: DataTypes.DATE,
+            allowNull: false,
+            validate: {
+                len: [1]
+            }
+        }
+    });
+    Schedule.associate = function (models) {
+        Schedule.belongsTo(models.User, {
+            foreignKey: {
+                allowNull: false
+            }
+        });
+    };
+    return Schedule;
+};
--- reactpractice/src/index.js
+++
@@ -1,5 +0,0 @@
-import ReactDOM from 'react-dom';
-import Loginscreen from './Loginscreen';
-import 'bootstrap/dist/css/bootstrap.min.css';
-
-ReactDOM.render(<Loginscreen />, document.getElementById('root'));
